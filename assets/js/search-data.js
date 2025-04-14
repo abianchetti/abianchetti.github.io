44710572e22194a2df80f2b4ecb6d112d07eebca
abianchetti <abianchetti@gmail.com>
2025-04-14 16:19:10 +0000

    Deploying to gh-pages from @ abianchetti/abianchetti.github.io@d3830f5787473bb291e9c7c47d8b37d2b056a1fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is my CV. You can also download a pdf version of it.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
