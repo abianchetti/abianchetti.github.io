08bb7b726d7fdd694a9b2fb0152749f829de285e
abianchetti <abianchetti@gmail.com>
2025-04-14 14:26:35 +0000

    Deploying to gh-pages from @ abianchetti/abianchetti.github.io@7800434c055d2bd2bd7b712e1a1ab50b44a6f43a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,7 +459,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%62%69%61%6E%63%68%65%74%74%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -467,6 +467,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/arturobianchetti", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-6480-1550", "_blank");
         },
       },{
         id: 'social-rss',
@@ -480,14 +494,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=WsxgdKwAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
